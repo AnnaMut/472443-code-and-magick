9f3037adc73cd307d6cce55fcf94768bbc72fb21
AnnaMut <butorovaanna@yandex.ru>
2018-04-03 17:10:44 +0300

    Поменяла переменную - значение цветаб откорректировала наименования

--- js/stat.js
+++ js/stat.js
@@ -6,7 +6,7 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var GAP = 10;
-var FONT_GAP = '16px PT Mono';
+var TEXT_STYLE = '16px PT Mono';
 var CLOUD_COLOR = '#fff';
 var TEXT_COLOR = '#000';
 var VIVA_TEXT = 'Ура вы победили!';
@@ -15,8 +15,7 @@ var GAP_BAR = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var SELF_BAR_COLOR = 'rgba(255, 0, 0, 1)';
-var BLUE_BAR_1 = 'rgba(0, 0, 255, ';
-var BLUE_BAR_2 = ')';
+var OTHER_USER_BAR_COLOR = '0, 0, 255';
 var NAMES_ARE_YOU = 'Вы';
 
 var renderCloud = function (ctx, x, y, color) {
@@ -41,15 +40,15 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = TEXT_COLOR;
   var maxTime = getMaxElement(times);
-  ctx.fontSlyle = FONT_GAP;
+  ctx.fontSlyle = TEXT_STYLE;
   ctx.fillText(VIVA_TEXT, CLOUD_X + GAP, (CLOUD_Y + GAP) * 2);
   ctx.fillText(POIN_LIST_TEXT, CLOUD_X + GAP, (CLOUD_Y + GAP) * 3);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fontSlyle = FONT_GAP;
+    ctx.fontSlyle = TEXT_STYLE;
     ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_Y + BAR_HEIGHT + GAP_BAR + GAP + GAP - (BAR_HEIGHT * times[i] / maxTime));
 
-    var colorBar = (names[i] === NAMES_ARE_YOU) ? SELF_BAR_COLOR : BLUE_BAR_1 + (Math.random() * 0.8 + 0.2) + BLUE_BAR_2;
+    var colorBar = (names[i] === NAMES_ARE_YOU) ? SELF_BAR_COLOR : 'rgba(' + OTHER_USER_BAR_COLOR + ',' + (Math.random() * 0.8 + 0.2) + ')';
     ctx.fillStyle = colorBar;
     ctx.fillRect(CLOUD_X + GAP_BAR + ((BAR_WIDTH + GAP_BAR) * i), CLOUD_Y + BAR_HEIGHT + GAP_BAR + GAP + GAP + GAP - (BAR_HEIGHT * times[i] / maxTime), BAR_WIDTH, BAR_HEIGHT * times[i] / maxTime);
 
